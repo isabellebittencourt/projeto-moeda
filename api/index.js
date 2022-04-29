58a8a7658d0c37d21997de59e5d4703dfa90618f
Isabelle Bittencourt <isabelleandrade164@gmail.com>
2022-04-28 21:02:26 -0300

    Inserindo o Swagger

--- api/index.js
+++ api/index.js
@@ -1,15 +1,17 @@
+const swaggerUI = require('swagger-ui-express')
+
 const express= require('express');
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const moedaRoute = require('./routes/moedas');
 const cors = require('cors');
-
+const swaggerDocs = require ("./swagger.json")
 
 dotenv.config();
 app.use(express.json());
 app.use(cors())
-
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
      useUnifiedTopology: true,
