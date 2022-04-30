4e8badf4b648d81b02ccca4c380715de98bb59a7
felipedalperio <45234913+felipedalperio@users.noreply.github.com>
2022-04-29 22:33:39 -0300

    Implementing Repository Pattern

---
+++ api/custom_modules/sendJsonResponse.js
@@ -0,0 +1,13 @@
+
+/**
+ * Send json response with the given status code
+ * 
+ * @param {object} the response object
+ * @param {number} the http status to send
+ * @param {object} the json content
+ */
+ function sendJsonResponse (res, status, content) {
+    res.status(status).json(content);
+}
+
+module.exports = sendJsonResponse;
--- api/routes/moedas.js
+++ api/routes/moedas.js
@@ -1,59 +1,17 @@
 //Iniciando a rota:
 const router = require("express").Router();
-//recuperando o model de moeda:
-const Moeda = require("../models/Moeda");
+const moedaController = require('../controller/moedaController');
+
+const moedaCrl = new moedaController();
 
 //INSERT:
-router.post("/register", async(req,res)=>{
-    try{
-        const newMoeda = new Moeda({
-            moedaOrigem: req.body.moedaOrigem,
-            moedaConvercao : req.body.moedaConvercao,
-            valorMoeda : req.body.valorMoeda,
-            valorMoedaInteiro : req.body.valorMoedaInteiro,
-            valorMoedaConvertida :req.body.valorMoedaConvertida
-        });
-        //salvando no banco:
-        const moeda = await newMoeda.save();
-        //se tudo der certo:
-        res.status(200).json(moeda)
-    }catch(err){
-        res.status(500).json(err)
-    }
-});
-
+router.post("/register", moedaCrl.doCreateMoeda);
 //UPDATE:
-router.put("/:id", async (req,res)=>{
-  
-        try{
-            const updateMoeda = await Moeda.findByIdAndUpdate(req.params.id,{
-                $set : req.body
-            },{new:true});
-            res.status(200).json(updateMoeda);
-        }catch(err){
-            res.status(500).json(err)
-        }
-
-});
-
+router.put("/:id", moedaCrl.doUpdateMoeda);
 //DELETE:
-router.delete("/:id", async (req,res)=>{
-        try{
-               await Moeda.findByIdAndDelete(req.params.id);
-               res.status(200).json("Dados deletado com sucesso!");
-        }catch(err){
-            res.status(500).json(err)
-        }
-});
+router.delete("/:id", moedaCrl.doDeleteMoeda);
+//GET
+router.get("/",moedaCrl.doListMoedas)
 
-//GET :
-router.get("/", async (req,res)=>{
-        try{
-               let moedas = await Moeda.find();
-               res.status(200).json(moedas);
-        }catch(err){
-            res.status(500).json(err)
-        }
-});
 
 module.exports = router
