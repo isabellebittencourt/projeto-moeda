0ed8e423beb5bd9549e9f8bc7f20e7637f37e02a
Felipe Dalperio <45234913+felipedalperio@users.noreply.github.com>
2022-06-23 19:29:39 -0300

    Consumidor C#

---
+++ consumidor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace consumidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        //iniciando o connectionFactory:
+            ConnectionFactory factory = new ConnectionFactory{
+                HostName = "localhost"
+            };
+            //utilizando o using:
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel()){
+                    channel.QueueDeclare(
+                        queue : "fila_python",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete:false,
+                        arguments: null
+                    );
+                    
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, message) => {
+                        //pode ser var também
+                        byte[] body = message.Body.ToArray();
+                        var text = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(text);
+                    };
+                    
+                   channel.BasicConsume(queue : "fila_python",
+                                        autoAck : true,
+                                        consumer: consumer);
+                    
+                    Console.WriteLine("Fim do consumo");
+
+                }
+            }
+        }
+    }
+}
